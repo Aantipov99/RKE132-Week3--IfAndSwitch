c597452ebb7aa8b917515d886b908238f438d23c
Anna Antipova <aantipov@tktk.ee>
2023-05-08 13:06:37 +0300

    add week3

---
+++ DayOfTheWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int WeekDay =Convert.ToInt32 (DateTime.Now.DayOfWeek);
+
+if(WeekDay == 0)
+{
+    Console.WriteLine("It is a Sunday!");
+ } 
+else if(WeekDay == 1)
+{
+    Console.WriteLine("It is a Monday!");
+}
+else if (WeekDay == 2)
+{
+    Console.WriteLine("It is Tuesday!");
+}
+else if (WeekDay == 3)
+{
+   Console.WriteLine("It is a Wednesday!");
+}
+else if (WeekDay == 4)
+{
+    Console.WriteLine("It is a Thursday!");
+}
+else if (WeekDay == 5)
+{
+    Console.WriteLine("It is a Friday!");
+}
+else
+{
+    Console.WriteLine("It is a Saturday!");
+}
---
+++ WEEK3IfAndSwitch/Program.cs
@@ -0,0 +1,10 @@
+﻿//moodulus-%
+
+int a = 24;
+int b = 8;
+
+int c = b % a; //+/-/* - /
+Console.WriteLine(c);
+
+
+
